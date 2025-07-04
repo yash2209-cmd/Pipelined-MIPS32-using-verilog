6e46ffccd4b1fe52216920c1ae3b69b5f366fdd7
yash2209-cmd <yash.2209.yp@gmail.com>
2025-07-04 14:49:29 +0530

    Update mips32.v

--- mips32.v
+++ mips32.v
@@ -28,16 +28,16 @@ always @(posedge clk1)         //IF stage
     begin
         if(((EX_MEM_IR[31:26] == BEQZ) && (EX_MEM_cond == 1)) || ((EX_MEM_IR[31:26] == BNEQZ) && (EX_MEM_cond == 0)))
         begin
-            IF_ID_IR <= #2 Mem[EX_MEM_ALUout];
-            TAKEN_BRANCH <= #2 1'b1;
-            IF_ID_NPC <= #2 EX_MEM_ALUout + 1;
-            PC <= #2 EX_MEM_ALUout + 1;
+            IF_ID_IR <=  Mem[EX_MEM_ALUout];
+            TAKEN_BRANCH <=  1'b1;
+            IF_ID_NPC <=  EX_MEM_ALUout + 1;
+            PC <=  EX_MEM_ALUout + 1;
         end
         else
         begin
-            IF_ID_IR <= #2 Mem[PC];
-            IF_ID_NPC <= #2 PC + 1;
-            PC <= #2 PC + 1;
+            IF_ID_IR <=  Mem[PC];
+            IF_ID_NPC <=  PC + 1;
+            PC <=  PC + 1;
         end 
     end
 
@@ -45,60 +45,60 @@ always @(posedge clk2)        //ID stage
         if(HALTED == 0)
         begin
             if(IF_ID_IR[25:21] == 5'b00000) ID_EX_A <= 0;
-            else ID_EX_A <= #2 Reg[IF_ID_IR[25:21]];  //rs
+            else ID_EX_A <=  Reg[IF_ID_IR[25:21]];  //rs
 
             if(IF_ID_IR[20:16] == 5'b00000) ID_EX_B <= 0;
-            else ID_EX_B <= #2 Reg[IF_ID_IR[20:16]];  //rt
+            else ID_EX_B <=  Reg[IF_ID_IR[20:16]];  //rt
 
-            ID_EX_NPC <= #2 IF_ID_NPC;
-            ID_EX_IR <= #2 IF_ID_IR;
-            ID_EX_Imm <= #2 {{16{IF_ID_IR[15]}},{IF_ID_IR[15:0]}};
+            ID_EX_NPC <=  IF_ID_NPC;
+            ID_EX_IR <=  IF_ID_IR;
+            ID_EX_Imm <=  {{16{IF_ID_IR[15]}},{IF_ID_IR[15:0]}};
 
             case(IF_ID_IR[31:26])
-                ADD,SUB,AND,OR,SLT,MUL: ID_EX_type <= #2 RR_ALU;
-                ADDI,SUBI,SLTI: ID_EX_type <= #2 RM_ALU;
-                LW: ID_EX_type <= #2 LOAD;
-                SW: ID_EX_type <= #2 STORE;
-                BNEQZ,BEQZ: ID_EX_type <= #2 BRANCH;
-                HLT: ID_EX_type <= #2 HALT;
-                default: ID_EX_type <= #2 HALT;
+                ADD,SUB,AND,OR,SLT,MUL: ID_EX_type <=  RR_ALU;
+                ADDI,SUBI,SLTI: ID_EX_type <=  RM_ALU;
+                LW: ID_EX_type <=  LOAD;
+                SW: ID_EX_type <=  STORE;
+                BNEQZ,BEQZ: ID_EX_type <=  BRANCH;
+                HLT: ID_EX_type <=  HALT;
+                default: ID_EX_type <=  HALT;
             endcase
         end
 
 always @(posedge clk1)        //EX stage
         if(HALTED == 0)
         begin
-            EX_MEM_type <= #2 ID_EX_type;
-            EX_MEM_IR <= #2 ID_EX_IR;
-            TAKEN_BRANCH <= #2 0;
+            EX_MEM_type <=  ID_EX_type;
+            EX_MEM_IR <=  ID_EX_IR;
+            TAKEN_BRANCH <=  0;
 
             case(ID_EX_type)
             RR_ALU: begin
                 case(ID_EX_IR[31:26])    //opcode
-                 ADD: EX_MEM_ALUout <= #2 ID_EX_A + ID_EX_B;
-                 SUB: EX_MEM_ALUout <= #2 ID_EX_A - ID_EX_B;
-                 AND: EX_MEM_ALUout <= #2 ID_EX_A & ID_EX_B;
-                 OR:  EX_MEM_ALUout <= #2 ID_EX_A | ID_EX_B;
-                 SLT: EX_MEM_ALUout <= #2 ID_EX_A < ID_EX_B;
-                 MUL: EX_MEM_ALUout <= #2 ID_EX_A * ID_EX_B;
-                 default: EX_MEM_ALUout <= #2 32'hxxxxxxxx;
+                 ADD: EX_MEM_ALUout <=  ID_EX_A + ID_EX_B;
+                 SUB: EX_MEM_ALUout <=  ID_EX_A - ID_EX_B;
+                 AND: EX_MEM_ALUout <=  ID_EX_A & ID_EX_B;
+                 OR:  EX_MEM_ALUout <=  ID_EX_A | ID_EX_B;
+                 SLT: EX_MEM_ALUout <=  ID_EX_A < ID_EX_B;
+                 MUL: EX_MEM_ALUout <=  ID_EX_A * ID_EX_B;
+                 default: EX_MEM_ALUout <=  32'hxxxxxxxx;
                 endcase
             end
             RM_ALU: begin
                 case(ID_EX_IR[31:26])    //opcode
-                ADDI: EX_MEM_ALUout <= #2 ID_EX_A + ID_EX_Imm;
-                SUBI: EX_MEM_ALUout <= #2 ID_EX_A - ID_EX_Imm;
-                SLTI: EX_MEM_ALUout <= #2 ID_EX_A < ID_EX_Imm;
-                default: EX_MEM_ALUout <= #2 32'hxxxxxxxx;
+                ADDI: EX_MEM_ALUout <=  ID_EX_A + ID_EX_Imm;
+                SUBI: EX_MEM_ALUout <=  ID_EX_A - ID_EX_Imm;
+                SLTI: EX_MEM_ALUout <=  ID_EX_A < ID_EX_Imm;
+                default: EX_MEM_ALUout <=  32'hxxxxxxxx;
                 endcase
             end
             LOAD,STORE: begin
-                EX_MEM_ALUout <= #2 ID_EX_A + ID_EX_Imm;
-                EX_MEM_B <= #2 ID_EX_B;
+                EX_MEM_ALUout <=  ID_EX_A + ID_EX_Imm;
+                EX_MEM_B <=  ID_EX_B;
             end
             BRANCH: begin
-                 EX_MEM_ALUout <= #2 ID_EX_NPC + ID_EX_Imm;
-                 EX_MEM_cond <= #2 (ID_EX_A == 0);
+                 EX_MEM_ALUout <=  ID_EX_NPC + ID_EX_Imm;
+                 EX_MEM_cond <=  (ID_EX_A == 0);
             end
             endcase
         end
@@ -106,14 +106,14 @@ always @(posedge clk1)        //EX stage
 always @(posedge clk2)        //MEM stage
         if(HALTED == 0)
         begin
-            MEM_WB_type <= #2 EX_MEM_type;
-            MEM_WB_IR <= #2 EX_MEM_IR;
+            MEM_WB_type <=  EX_MEM_type;
+            MEM_WB_IR <=  EX_MEM_IR;
 
             case(EX_MEM_type)
-            RR_ALU,RM_ALU: MEM_WB_ALUout <= #2 EX_MEM_ALUout;
-            LOAD: MEM_WB_LMD <= #2 Mem[EX_MEM_ALUout];
+            RR_ALU,RM_ALU: MEM_WB_ALUout <=  EX_MEM_ALUout;
+            LOAD: MEM_WB_LMD <=  Mem[EX_MEM_ALUout];
             STORE: if(TAKEN_BRANCH == 0)    //disable write
-                      Mem[EX_MEM_ALUout] <= #2 EX_MEM_B;
+                      Mem[EX_MEM_ALUout] <=  EX_MEM_B;
             endcase
         end
 
@@ -121,10 +121,10 @@ always @(posedge clk1)        //WB stage
         begin
             if(TAKEN_BRANCH == 0)    //disable write if branch taken
             case(MEM_WB_type)
-            RR_ALU: Reg[MEM_WB_IR[15:11]] <= #2 MEM_WB_ALUout;   //rd
-            RM_ALU: Reg[MEM_WB_IR[20:16]] <= #2 MEM_WB_ALUout;   //rt
-            LOAD: Reg[MEM_WB_IR[20:16]] <= #2 MEM_WB_LMD;    //rt
-            HALT: HALTED <= #2 1'b1;
+            RR_ALU: Reg[MEM_WB_IR[15:11]] <=  MEM_WB_ALUout;   //rd
+            RM_ALU: Reg[MEM_WB_IR[20:16]] <=  MEM_WB_ALUout;   //rt
+            LOAD: Reg[MEM_WB_IR[20:16]] <=  MEM_WB_LMD;    //rt
+            HALT: HALTED <=  1'b1;
             endcase
         end
 
